5a8fb6833ba22ceb4a1847e16a7e32f5c03d0e6e
Andrey Skryl <ya.skryl@yandex.ru>
2016-08-19 16:04:56 +0600

    Добавлена фильтрация для задач.

--- frontend/app/views/listOfTask/ListOfTasksController.js
+++ frontend/app/views/listOfTask/ListOfTasksController.js
@@ -7,6 +7,8 @@
 				$scope.listOfTasks = {};
 				$scope.subscribers = [];
 
+				$scope.statusOfTasks = '';
+
 				var guidOfListOfTasks = $stateParams.guidOfListOfTasks;
 
 				var syncModelWithServer = function () {
@@ -44,74 +46,14 @@
 						animation: true,
 						templateUrl: 'views/listOfTask/modalDialog/ModalDialogAddColleague.tpl.html',
 						controller: 'ModalDialogAddColleagueCtrl',
-						size: size,
-						resolve: {
-							/*items: function () {
-								return $scope.items;
-							}*/
-						}
+						size: size
 					});
 
-					modalInstance.result.then(function (/*selectedItem*/) {
-						/*$scope.selected = selectedItem;*/
-					}, function () {
+					modalInstance.result.then(function () {}, function () {
 						$log.info('Modal dismissed at: ' + new Date());
 					});
 				};
 
 			}
 		]);
-
-	/*angular.module('todoListApp').controller('ModalDemoCtrl', ['$scope', '$uibModal', '$log', function ($scope, $uibModal, $log) {
-
-		$scope.items = ['item1', 'item2', 'item3'];
-
-		$scope.animationsEnabled = true;
-
-		$scope.open = function (size) {
-
-			var modalInstance = $uibModal.open({
-				animation: $scope.animationsEnabled,
-				templateUrl: 'myModalContent.html',
-				controller: 'ModalInstanceCtrl',
-				size: size,
-				resolve: {
-					items: function () {
-						return $scope.items;
-					}
-				}
-			});
-
-			modalInstance.result.then(function (selectedItem) {
-				$scope.selected = selectedItem;
-			}, function () {
-				$log.info('Modal dismissed at: ' + new Date());
-			});
-		};
-
-		$scope.toggleAnimation = function () {
-			$scope.animationsEnabled = !$scope.animationsEnabled;
-		};
-
-	}]);*/
-
-	// Please note that $uibModalInstance represents a modal window (instance) dependency.
-	// It is not the same as the $uibModal service used above.
-
-	/*angular.module('todoListApp').controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', 'items', function ($scope, $uibModalInstance, items) {
-
-		$scope.items = items;
-		$scope.selected = {
-			item: $scope.items[0]
-		};
-
-		$scope.ok = function () {
-			$uibModalInstance.close($scope.selected.item);
-		};
-
-		$scope.cancel = function () {
-			$uibModalInstance.dismiss('cancel');
-		};
-
-	}]);*/
 })();
